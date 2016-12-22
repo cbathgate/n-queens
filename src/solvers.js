afa7aaea7d5de71746dc9c01d37984e342685951
cbathgate <c.bathgate1@gmail.com>
2016-12-21 21:10:57 -0800

    Implement first attempt at findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,10 +16,40 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  if (n === 1) {
+    return [[1]];
+  }
 
+  var solution = new Board({n: n});
+  var blankArray = solution.rows()[0].slice();
+  var result = [];
+  
+  var createTree = function(matrix) {
+    var newNode = new Board(matrix);
+    var setArray = blankArray.slice();
+
+    for (var row = 0; row < newNode.rows().length; row++) {
+      for (var col = 0; col < newNode.rows().length; col++) {
+        var test = newNode.rows();
+        test[row][col] = 1;
+        test = new Board(test);
+        
+        if (!test.hasRowConflictAt(row) && !test.hasColConflictAt(col)) {
+          setArray[col] = 1;
+          newNode.set(row, setArray);
+          result = newNode;
+          return createTree(newNode.rows());
+        } else {
+          return;
+        }
+      }
+    }
+    // return matrix;
+  };
+
+  createTree(solution.rows());
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return result;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
